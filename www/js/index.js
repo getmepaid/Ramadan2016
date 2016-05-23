270f82d7b1053ac179695553a74ede4b9d97b12e
getmepaid <mohamedalayyat@gmail.com>
2016-05-23 03:53:43 +0200

    js fix

--- www/js/index.js
+++ www/js/index.js
@@ -19,7 +19,7 @@ var app = {
        // app.receivedEvent('deviceready');
 	   //window.open('http://youfeellike.sharedin.net', '_blank', 'location=no')
 	   var inAppBrowserbRef;
-       inAppBrowserbRef = window.open('http://movieme.ovplatform.tk', '_blank', 'location=no,toolbar=no');
+       inAppBrowserbRef = window.open('http://ramadan2016.ovplatform.tk', '_blank', 'location=no,toolbar=no');
          inAppBrowserbRef.addEventListener('loadstart', inAppBrowserbLoadStart);
          inAppBrowserbRef.addEventListener('loadstop', inAppBrowserbLoadStop);
          inAppBrowserbRef.addEventListener('loaderror', inAppBrowserbLoadError);
